0568cd4d0b18c5d01aceecbfd7f6376914053fd3
Pro Dev <prodev9999@gmail.com>
2019-10-11 05:19:10 +0800

    Update incoming call treatment

--- app/src/main/java/com/silencer/PhoneStateReceiver.kt
+++ app/src/main/java/com/silencer/PhoneStateReceiver.kt
@@ -4,14 +4,24 @@ import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.telephony.PhoneStateListener
+import android.os.Handler
 import android.telephony.TelephonyManager
+import android.util.Log
 
 class PhoneStateReceiver : BroadcastReceiver() {
 
     @SuppressLint("UnsafeProtectedBroadcastReceiver")
     override fun onReceive(context: Context, intent: Intent) {
-        val telephony = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-        telephony.listen(PhoneStateChangeListener(context), PhoneStateListener.LISTEN_CALL_STATE)
+        Log.d("Silencer", "incoming Call")
+
+        val stateStr = intent.extras!!.getString(TelephonyManager.EXTRA_STATE)!!
+        if (stateStr.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+            Handler().postDelayed({
+                Engine.silence(context)
+            }, 500)
+        }
+
+//        val telephony = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+//        telephony.listen(PhoneStateChangeListener(context), PhoneStateListener.LISTEN_CALL_STATE)
     }
 }
